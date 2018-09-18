467b5d2e266f7bd65260dd67ca37008275baf2e3
高立人 <gaoliren@gaolirendeMacBook-Pro.local>
2018-09-18 09:01:00 +0800

    today's homework

---
+++ dateframeworkintern/2017.9.18/2017.9.18/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2017.9.18
+//
+//  Created by 高立人 on 2018/9/18.
+//  Copyright © 2018年 高立人. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- dateframeworkintern/2018.9.17/2018.9.17/main.cpp
+++ dateframeworkintern/2018.9.17/2018.9.17/main.cpp
@@ -1,15 +1,62 @@
-//
-//  main.cpp
-//  2018.9.17
-//
-//  Created by 高立人 on 2018/9/17.
-//  Copyright © 2018年 高立人. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <memory.h>
+using namespace std;
+int parent[50010];
+int relation[50010];
+int n,k;
+int getbase(int a)
+{
+    if(parent[a]!=a)
+    {
+        int tmp=parent[a];
+        parent[a]=getbase(parent[a]);
+        relation[a]=(relation[a]+relation[tmp])%3;
+        
+    }
+    return parent[a];
+}
+void merge(int a,int b,int r)
+{
+    int p1=getbase(a);
+    int p2=getbase(b);
+    if(p1!=p2)
+    {
+        parent[p1]=p2;
+        relation[p1]=(r-relation[a]+3+relation[b])%3;
+    }
+}
+bool check(int a,int b,int c)
+{
+    if(a>n||b>n)
+        return 0;
+    if(c==1&&a==b)
+        return 0;
+    int p1=getbase(a);
+    int p2=getbase(b);
+    if(p1==p2)
+        return c==(relation[a]-relation[b]%3+3)%3;
+    else
+        return 1;
+    
+}
+int main() {
+    cin>>n>>k;
+    for(int i=0;i<n;++i)
+    {
+        relation[i]=0;
+        parent[i]=i;
+    }
+    int D,X,Y;
+    int sum=0;
+    while(k--)
+    {
+        scanf("%d %d %d",&D,&X,&Y);
+        D--;
+        if(check(X,Y,D))
+            merge(X,Y,D);
+        else
+            sum++;
+    }
+    printf("%d",sum);
     return 0;
 }

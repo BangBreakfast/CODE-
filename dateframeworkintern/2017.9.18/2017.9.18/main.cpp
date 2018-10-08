c021900b35c6225a366ce975917c32cdd51ddeae
高立人 <gaoliren@gaolirendemacbook-pro.local>
2018-10-08 18:32:34 +0800

    today's code

--- dateframeworkintern/2017.9.18/2017.9.18/main.cpp
+++ dateframeworkintern/2017.9.18/2017.9.18/main.cpp
@@ -1,15 +1,68 @@
-//
-//  main.cpp
-//  2017.9.18
-//
-//  Created by 高立人 on 2018/9/18.
-//  Copyright © 2018年 高立人. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int parent[50010];
+int sexual[50010];
+int getroot(int a)
+{
+    if(parent[a]!=a)
+    {
+        int tmp=parent[a];
+        parent[a]=getroot(parent[a]);
+        sexual[a]=(sexual[a]+sexual[tmp])%2;
+    }
+    else
+        sexual[a]=0;
+    return parent[a];
+}
+bool check(int a,int b)
+{
+    int p1=getroot(a);
+    int p2=getroot(b);
+    if(p1==p2&&sexual[a]==sexual[b])
+        return 0;
+    return 1;
+}
+void merge(int a,int b)
+{
+    int p1=getroot(a);
+    int p2=getroot(b);
+    if(p1!=p2)
+    {
+        parent[p2]=p1;
+        sexual[p2]=(sexual[a]+1-sexual[b]+2)%2;
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;++i)
+    {
+        int bg,it;
+        cin>>bg>>it;
+        bool tmp=0;
+        for(int j=0;j<=bg;++j)
+        {
+            parent[j]=j;
+            sexual[j]=0;
+        }
+        while(it--)
+        {
+            int x,y;
+            scanf("%d %d",&x,&y);
+            if(check(x,y))
+                merge(x,y);
+            else
+            {
+                tmp=1;
+                continue;
+            }
+        }
+        printf("Scenario #%d:\n",i);
+        if (tmp)
+            printf("Suspicious bugs found!\n");
+        else
+            printf("No suspicious bugs found!\n");
+        printf("\n");
+    }
     return 0;
 }
